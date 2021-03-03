634613654618b8182fa358f073bdde567ca7d8c3
Cameron Liddell <camerongliddell@gmail.com>
2021-03-03 17:37:22 +0000

    Changed image locations

--- CamBotButHesFullOfDumbShite/Services/CommandHandler.cs
+++ CamBotButHesFullOfDumbShite/Services/CommandHandler.cs
@@ -72,7 +72,7 @@ namespace CamBotButHesFullOfDumbShite.Services
                 return;
             }
 
-            await context.Channel.SendFileAsync(@"/home/pi/CamBotButHesFullOfDumbShite/CamBot_Sad.png", "Error, something went wrong :(");
+            await context.Channel.SendFileAsync(@"/home/pi/CamBot_Sad.png", "Error, something went wrong :(");
         }
     }
 }
